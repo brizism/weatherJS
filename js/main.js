8369110ae90b8e19a03fd17c3d27a7b9317ac8a6
brizism <montielbriseida@gmail.com>
2018-02-19 16:28:11 -0500

    save to local storage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+// Init storage
+const storage = new Storage();
+
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
 // Init weather object
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 
 // Init UI
 const ui = new UI();
@@ -7,7 +13,25 @@ const ui = new UI();
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation('Bronx', 'NY')
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', e => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  // Change location
+  weather.changeLocation(city, state)
+
+  // Set location in local storage
+  storage.setLocationData(city, state);
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+})
+
+
 
 function getWeather(){
   weather.getWeather()
